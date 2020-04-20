6e426ded80730c7d2d8a6aaf17d4d0b6eb242c21
Coding For Summer <hello@terrysummerbell.com>
2020-04-20 20:46:56 +0100

    Hashes ex.1

---
+++ hashes/hashes-ex1.rb
@@ -0,0 +1,14 @@
+family = {  uncles: ["bob", "joe", "steve"],
+            sisters: ["jane", "jill", "beth"],
+            brothers: ["frank", "rob", "david"],
+            aunts: ["mary", "Sally", "susan"]
+          }
+          
+immediate_family = family.select do |k, v|
+  k == :sisters || k == :brothers
+end
+# => {:sisters=>["jane", "jill", "beth"], :brothers=>["frank", "rob", "david"]}
+
+arr = immediate_family.values.flatten
+# => ["jane", "jill", "beth", "frank", "rob", "david"]
+p arr
